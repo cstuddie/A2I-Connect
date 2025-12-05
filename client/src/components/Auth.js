80396932e1bfc0d6be79341c2b982ee82c580855
Caleb Studdie <calebstuddie@gmail.com>
2025-12-05 08:41:47 -0600

    Reapply "Register/Login Overhaul"
    This reverts commit 9238c3c13ff51108a168bc016ac34ec93a68c95b.

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -48,7 +48,7 @@ function Auth() {
     }
   
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('http://localhost:3001/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
