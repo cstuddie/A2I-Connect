d53033c1779475d47be73778ac2bf2b68ddc08b6
Tnguyen0525 <71985660+Tnguyen0525@users.noreply.github.com>
2026-05-01 13:51:45 -0500

    restores notification route

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,7 @@ import AdminUsers from "./components/Admin/AdminUsers";
 import AdminEvents from "./components/Admin/AdminEvents";
 import UserProfile from "./components/UserProfile";
 import Inbox from "./components/Inbox"
+import NotificationsPage from "./components/NotificationsPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/Base.css';
 
@@ -64,6 +65,10 @@ const router = createBrowserRouter([
   {
     path:'/Calendar',
     element: <Calendar />
+  },
+  {
+    path:'/Notifications',
+    element: <NotificationsPage />
   },
   // Admin routes
   {
